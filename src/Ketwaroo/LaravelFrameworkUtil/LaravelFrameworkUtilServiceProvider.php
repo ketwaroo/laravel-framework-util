d48d943ab1847e4ca234a6c911c816506eaec19d
Yaasir Ketwaroo <ketwaroo.yaasir@gmail.com>
2014-11-25 08:41:09 -0500

    initial commit

---
+++ src/Ketwaroo/LaravelFrameworkUtil/LaravelFrameworkUtilServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Ketwaroo\LaravelFrameworkUtil;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFrameworkUtilServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
